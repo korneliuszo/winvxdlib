aca80a6e223ef3a35a287f51f53bd115c16fd34f
Korneliusz Osmenda <korneliuszo@gmail.com>
2023-07-18 20:36:03 +0200

    Add needed macros so vxd file works

--- headers/include/gcc.h
+++ headers/include/gcc.h
@@ -10,6 +10,28 @@
 
 #define DLL_PUBLIC __attribute__ ((visibility ("default")))
 
+#define VXD_STRINGIFY2(x)   #x
+#define VXD_STRINGIFY(x)    VXD_STRINGIFY2(x)
 
+#define VXD_AsmCall(dev,srv) \
+        "int    $0x20\n"                                                        \
+        ".word  " VXD_STRINGIFY(srv) "\n"                                       \
+        ".word  " VXD_STRINGIFY(dev) "\n"
+
+#if (__GNUC__ >= 6 && __GNUC_MINOR__ >= 1) || (__GNUC__ >= 7)
+# define GCC_INLINE_ASM_SUPPORTS_cc_OUTPUT
+#endif
+
+#define __asm__ asm volatile
+
+typedef uint32_t vxd_vm_handle_t;
+
+typedef uint32_t vxd_semaphore_handle_t;
+
+typedef uint32_t vxd_global_event_handle_t;
+typedef uint32_t vxd_vm_event_handle_t;
+typedef uint32_t vxd_priority_vm_event_handle_t;
+
+typedef uint32_t vxd_timeout_handle_t;
 
 #endif /* HEADERS_GCC_H_ */
