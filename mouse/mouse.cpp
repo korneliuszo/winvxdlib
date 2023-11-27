c42b115cea1f0c30db167f07eb985bc53cb05604
Korneliusz Osmenda <korneliuszo@gmail.com>
2023-11-27 19:52:19 +0100

    Semaphorize, debugize, clickize mouse

--- mouse/mouse.cpp
+++ mouse/mouse.cpp
@@ -34,6 +34,13 @@ bool Mouse::Init(uint32_t sysVM, uint32_t crs)
 	End_Nest_Exec();
 	Restore_Client_State(&save);
 	VMD_Owner = sysVM;
+
+	Create_Semaphore__response resp;
+	resp = Create_Semaphore(1);
+	shandle = resp.Semaphore;
+	resp = Create_Semaphore(0);
+	rhandle = resp.Semaphore;
+
 	return exists;
 }
 
@@ -48,6 +55,10 @@ void Mouse::Focus(uint32_t VID, uint32_t flags, uint32_t VM)
 bool Mouse::do_setting(uint32_t crs)
 {
 	volatile Client_Reg_Struc *pCRS = (Client_Reg_Struc *)crs;
+
+#ifndef NDEBUG
+	//Out_Debug_String("do_setting ENTER\r\n");
+#endif
 
 	if(!sizex)
 	{
@@ -67,17 +78,30 @@ bool Mouse::do_setting(uint32_t crs)
 
 		End_Nest_Exec();
 		Restore_Client_State(&save);
+#ifndef NDEBUG
 		char buff[30];
 		snprintf(buff,30,"MOUSE reg: %d %d\r\n",sizex,sizey);
 		Out_Debug_String(buff);
+#endif
 	}
+
+#ifndef NDEBUG
+	if(0)
+	{
+		char buff[30];
+		snprintf(buff,30,"MOUSE c: %d\r\n",mouseclicked);
+		Out_Debug_String(buff);
+	}
+#endif
 
 	Client_Reg_Struc save;
 	Save_Client_State(&save);
+	Enable_VM_Ints();
+
 	Begin_Nest_V86_Exec();
 
 	pCRS->Client_EAX = 1;
-	pCRS->Client_EDX = 0;
+	pCRS->Client_EDX = mouseclicked ? 1 : 0;
 	pCRS->Client_ESI = 0;
 	pCRS->Client_EBX = (mouseposx*(sizex+1))>>16;
 	pCRS->Client_ECX = (mouseposy*(sizey+1))>>16;
@@ -86,23 +110,43 @@ bool Mouse::do_setting(uint32_t crs)
 	Resume_Exec();
 	End_Nest_Exec();
 	Restore_Client_State(&save);
+	Signal_Semaphore(rhandle);
+
+#ifndef NDEBUG
+	//Out_Debug_String("do_setting EXIT\r\n");
+#endif
 
 	return 1;
 }
 
 
-void Mouse::Set_Mouse_Position(uint16_t x, uint16_t y)
+void Mouse::Set_Mouse_Position(uint16_t x, uint16_t y, bool clicked)
 {
+#ifndef NDEBUG
+	//Out_Debug_String("set_pos ENTER\r\n");
+#endif
+	Wait_Semaphore(shandle, Block_Svc_If_Ints_Locked | Block_Enable_Ints);
+#ifndef NDEBUG
+	//Out_Debug_String("set_pos LOCK\r\n");
+#endif
 	mouseposx = x;
 	mouseposy = y;
+	mouseclicked = clicked;
 	Call_Priority_VM_Event(
 			Time_Critical_Boost,
 			VMD_Owner,
-			PEF_Wait_For_STI | PEF_Always_Sched,
+			0,
 			this,
 			(const void *)single_vxd_control_hanlder<
 				Cwrap<Mouse,&Mouse::do_setting,bool,uint32_t>,
 				'd','B'>,
 			0);
-
+#ifndef NDEBUG
+	//Out_Debug_String("set_pos PREWAIT\r\n");
+#endif
+	Wait_Semaphore(rhandle, Block_Svc_If_Ints_Locked | Block_Enable_Ints);
+	Signal_Semaphore(shandle);
+#ifndef NDEBUG
+	//Out_Debug_String("set_pos EXIT\r\n");
+#endif
 }
--- mouse/mouse.hpp
+++ mouse/mouse.hpp
@@ -9,6 +9,8 @@
 #define MOUSE_MOUSE_HPP_
 
 #include <stdint.h>
+#include <vmm.hpp>
+#include <gcc.h>
 
 class Mouse {
 
@@ -17,13 +19,16 @@ class Mouse {
 	uint32_t VMD_Owner;
 	uint16_t sizex;
 	uint16_t sizey;
-	uint16_t mouseposx;
-	uint16_t mouseposy;
+	volatile uint16_t mouseposx;
+	volatile uint16_t mouseposy;
+	volatile bool mouseclicked;
+	vxd_semaphore_handle_t shandle;
+	vxd_semaphore_handle_t rhandle;
 	bool do_setting(uint32_t crs);
 public:
 	bool Init(uint32_t sysVM, uint32_t crs);
 	void Focus(uint32_t VID, uint32_t flags, uint32_t VM);
-	void Set_Mouse_Position(uint16_t x, uint16_t y);
+	void Set_Mouse_Position(uint16_t x, uint16_t y, bool clicked = false);
 };
 
 
