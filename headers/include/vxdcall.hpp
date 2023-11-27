c413b8099e7ded89d8c7a15029e5104a93493ebf
Korneliusz Osmenda <korneliuszo@gmail.com>
2023-11-27 19:51:33 +0100

    Add VTD_Get_Real_Time

--- headers/include/vxdcall.hpp
+++ headers/include/vxdcall.hpp
@@ -8,6 +8,7 @@
 #ifndef HEADERS_INCLUDE_VXDCALL_HPP_
 #define HEADERS_INCLUDE_VXDCALL_HPP_
 
+#include <stdint.h>
 
 static inline void Out_Debug_String(const char * str)
 {
@@ -25,7 +26,19 @@ static inline void Out_Debug_String(const char * str)
 }
 
 
-
+static inline uint64_t VTD_Get_Real_Time()
+{
+	uint32_t hi,lo;
+	asm volatile (
+			"pushf\n\t"
+			"int $0x20\n\t"
+			".word 0x0007\n\t"
+			".word 0x0005\n\t"
+			"popf\n\t"
+			: "=edx"(hi),"=eax"(lo)
+	);
+	return (((uint64_t)hi)<<32) | lo;
+}
 
 
 
